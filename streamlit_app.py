cf6a14c1af56882aa768a9dee76bb58812a9fac5
Rio Dwi Putra Perkasa <perkasaid.rio@gmail.com>
2023-06-29 11:42:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,4 +68,3 @@ if streamlit.button('Add a Fruit to the List'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   my_cnx.close()
   streamlit.text(back_from_function)
-streamlit.write('Thanks for adding ', add_my_fruit)
